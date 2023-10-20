222fe26762e2bf4c4df4204c8be3a95cc0a092a8
Nurdoka <nurdoolot93@gmail.com>
2023-10-21 04:31:37 +0600

    setup

---
+++ src/com/skilldistillery/jets/app/HelicoptersApp.java
@@ -0,0 +1,19 @@
+package com.skilldistillery.jets.app;
+
+public class HelicoptersApp {
+
+	public static void main(String[] args) {
+		HelicoptersApp app = new HelicoptersApp();
+		app.run();
+
+	}
+	
+	public void run() {
+		
+	}
+	
+	public void displayMenu() {
+		
+	}
+
+}
---
+++ src/com/skilldistillery/jets/entities/Helicopters.java
@@ -0,0 +1,5 @@
+package com.skilldistillery.jets.entities;
+
+public class abstract Helicopters {
+	public abstract void fly();
+}
